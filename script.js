e521f32ec89f254f2fa742bfcb3bb85a3ba285ec
Ananya Lamba <ananyalamba2002@gmail.com>
2024-11-03 00:17:47 +0530

    js modified

--- script.js
+++ script.js
@@ -4,62 +4,74 @@ let sec = document.querySelector("#seconds");
 let minCounter = 0;
 let secCounter = 0;
 let hourCounter = 0;
+
 const start = document.querySelector("#start");
 const pause = document.querySelector("#stop");
 const reset = document.querySelector("#Reset");
+
 let setId;
+
 start.addEventListener("click", () => {
-    //start aur reset disable honge baki dono chalenge
+    // Disable start button, enable pause and reset
     start.disabled = true;
-    reset.disabled = true;
     pause.disabled = false;
-  setId = setInterval(function () {
-    secCounter++;
-    if(secCounter<10)
-    {
-        sec.innerHTML = `0${secCounter}`
-    }
-    else{
-        sec.innerHTML = secCounter;
-    }
-    if (secCounter > 58) {
-      minCounter++;
-      if(minCounter<10){
-        minutes.innerHTML = `0${minCounter}`
-      }
-      else{
-        minutes.innerHTML = minCounter;
-      }
-      secCounter = 0;
-    }
-    if (minCounter > 58) {
-      hourCounter++;
-      if(hourCounter<10){
-        hours.innerHTML = `0${hourCounter}`
-      }
-        else{
-            hours.innerHTML = hourCounter;
+    reset.disabled = false;
+
+    // Clear any existing interval before starting a new one
+    clearInterval(setId);
+
+    setId = setInterval(function () {
+        secCounter++;
+
+        // Display seconds with leading zero if less than 10
+        sec.innerHTML = secCounter < 10 ? `0${secCounter}` : secCounter;
+
+        // When seconds reach 60, increment minutes and reset seconds
+        if (secCounter > 59) {
+            minCounter++;
+            secCounter = 0;
+            sec.innerHTML = "00";
+
+            // Display minutes with leading zero if less than 10
+            minutes.innerHTML = minCounter < 10 ? `0${minCounter}` : minCounter;
         }
-      minCounter = 0;
-      secCounter = 0;
-    }
-  }, 1000);
+
+        // When minutes reach 60, increment hours and reset minutes
+        if (minCounter > 59) {
+            hourCounter++;
+            minCounter = 0;
+            minutes.innerHTML = "00";
+
+            // Display hours with leading zero if less than 10
+            hours.innerHTML = hourCounter < 10 ? `0${hourCounter}` : hourCounter;
+        }
+    }, 1000);
 });
+
 pause.addEventListener("click", () => {
-    //pause disable hoga baki dono chalenge
+    // Enable start and reset buttons, disable pause
     start.disabled = false;
     reset.disabled = false;
     pause.disabled = true;
-  clearInterval(setId);
+    clearInterval(setId);
 });
+
 reset.addEventListener("click", () => {
-    //start disable hoga aur pause enable
+    // Enable start button, disable pause
     start.disabled = false;
     pause.disabled = true;
-    minCounter=0;
-    secCounter=0;
-    hourCounter=0;
- hours.innerHTML = "00";
- minutes.innerHTML = "00";
- sec.innerHTML = "00";
+    reset.disabled = false;
+
+    // Reset all counters
+    minCounter = 0;
+    secCounter = 0;
+    hourCounter = 0;
+
+    // Reset display
+    hours.innerHTML = "00";
+    minutes.innerHTML = "00";
+    sec.innerHTML = "00";
+
+    // Clear the interval if the timer was running
+    clearInterval(setId);
 });
